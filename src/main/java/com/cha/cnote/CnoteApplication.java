dfe90de9cc276ebb168ae3dde7e0156b55f065bc
chacha86 <taejincha0619@gmail.com>
2024-10-26 16:11:27 +0900

    init

---
+++ src/main/java/com/cha/cnote/CnoteApplication.java
@@ -0,0 +1,13 @@
+package com.cha.cnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cha/cnote/CnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cha.cnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
